92ed71c4a0de70d846db6a223a3cab6f375bc505
Wenlin Huang <jackhwl@hotmail.com>
2019-05-02 09:03:21 -0400

    Adding Multiple Delegates to an Invocation List

---
+++ patterns/observer/csharp-events-delegates/2/Before/DelegatesAndEvents/DelegatesAndEvents/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesAndEvents
+{
+    public delegate void WorkPerformedHandler(int hours, WorkType workType);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var del1 = new WorkPerformedHandler(WorkPerformed1);
+            var del2 = new WorkPerformedHandler(WorkPerformed2);
+            var del3 = new WorkPerformedHandler(WorkPerformed3);
+
+            del1 += del2 + del3;
+            del1(10, WorkType.GenerateReports);
+
+            Console.ReadLine();
+        }
+
+        static void DoWork(WorkPerformedHandler del)
+        {
+            del(5, WorkType.GoToMeetings);
+        }
+
+        static void WorkPerformed1(int hours, WorkType workType)
+        {
+            Console.WriteLine("WorkPerformed1 called " + hours.ToString());
+            //return hours + 1;
+        }
+
+        static void WorkPerformed2(int hours, WorkType workType)
+        {
+            Console.WriteLine("WorkPerformed2 called " + hours.ToString());
+            //return hours + 2;
+        }
+
+        static void WorkPerformed3(int hours, WorkType workType)
+        {
+            Console.WriteLine("WorkPerformed3 called " + hours.ToString());
+            //return hours + 3;
+        }
+    }
+
+    public enum WorkType
+    {
+        GoToMeetings,
+        Golf,
+        GenerateReports
+    }
+}
