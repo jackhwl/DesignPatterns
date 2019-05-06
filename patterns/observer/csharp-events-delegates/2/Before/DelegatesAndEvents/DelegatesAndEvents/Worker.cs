7dd41479bb311d39ca3f6e27ac7a98f54e047499
Wenlin Huang <jackhwl@hotmail.com>
2019-05-06 06:28:05 -0400

    Raising Events

--- patterns/observer/csharp-events-delegates/2/Before/DelegatesAndEvents/DelegatesAndEvents/Worker.cs
+++ patterns/observer/csharp-events-delegates/2/Before/DelegatesAndEvents/DelegatesAndEvents/Worker.cs
@@ -13,7 +13,34 @@ namespace DelegatesAndEvents
         public event EventHandler WorkCompleted;
         public void DoWork(int hours, WorkType workType)
         {
+            // Do work here and notify consumer that work has been performed
+            //OnWorkPerformed(hours, workType);
 
+            for (int i=0; i< hours; i++)
+            {
+                OnWorkPerformed(i+1, workType);
+            }
+            OnWorkCompleted();
+        }
+        protected virtual void OnWorkPerformed(int hours, WorkType workType)
+        {
+            //if (WorkPerformed != null)
+            //{
+            //    WorkPerformed(hours, workType);
+            //}
+            //WorkPerformed?.Invoke(hours, workType);
+
+            //WorkPerformedHandler del = WorkPerformed as WorkPerformedHandler;
+            //if (del != null)   // Listensers are attached
+            //{
+            //    del(hours, workType);   // Raise Event
+            //}
+
+            (WorkPerformed as WorkPerformedHandler)?.Invoke(hours, workType);
+        }
+        protected virtual void OnWorkCompleted()
+        {
+            (WorkCompleted as EventHandler)?.Invoke(this, EventArgs.Empty);
         }
     }
 }
