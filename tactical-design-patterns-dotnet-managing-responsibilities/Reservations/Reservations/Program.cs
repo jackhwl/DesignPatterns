5a90443b5831964d136a5c91728a7aa12800428c
Wenlin Huang <jackhwl@hotmail.com>
2019-05-10 18:40:29 -0400

    Using Inversion of Control to Resolve Dependencies

--- tactical-design-patterns-dotnet-managing-responsibilities/Reservations/Reservations/Program.cs
+++ tactical-design-patterns-dotnet-managing-responsibilities/Reservations/Reservations/Program.cs
@@ -3,15 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Unity;
+using Unity.Lifetime;
 
 namespace Reservations
 {
     class Program
     {
+        static IUnityContainer InitializaIoCContainer()
+        {
+            return new UnityContainer()
+                .RegisterType<Application, Application>()
+                .RegisterType<IVacationPartFactory, VacationPartFactory>(new ContainerControlledLifetimeManager())
+                .RegisterType<IHotelSelector, HotelSector>(new ContainerControlledLifetimeManager())
+                .RegisterType<IHotelService, HotelService>(new ContainerControlledLifetimeManager())
+                .RegisterType<IAirplaneService, AirplaneService>(new ContainerControlledLifetimeManager());
+        }
         static void Main(string[] args)
         {
-            new Application(
-                new VacationPartFactory(new HotelSector(), new HotelService(), new AirplaneService()))
+            //new Application(
+            //    new VacationPartFactory(
+            //        new HotelSector(), 
+            //        new HotelService(), 
+            //        new AirplaneService()))
+            //    .Run();
+
+            InitializaIoCContainer()
+                .Resolve<Application>()
                 .Run();
             Console.ReadLine();
         }
