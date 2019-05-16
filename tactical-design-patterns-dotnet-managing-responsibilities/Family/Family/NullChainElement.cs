5c49587f1cad8ed3dbcb5ec27af2f90aafc95d0e
Wenlin Huang <jackhwl@hotmail.com>
2019-05-15 21:35:46 -0400

    add nullchainelement use thread safe singleton

--- tactical-design-patterns-dotnet-managing-responsibilities/Family/Family/NullChainElement.cs
+++ tactical-design-patterns-dotnet-managing-responsibilities/Family/Family/NullChainElement.cs
@@ -8,15 +8,24 @@ namespace Family.Common
 {
     public class NullChainElement : IChainElement
     {
-        private static IChainElement instance;
+        //private static IChainElement instance;
         private NullChainElement() { }
         public static IChainElement Instance
         {
             get
             {
-                if (instance == null) instance = new NullChainElement();
-                return instance;
+                //not thread safe
+                //if (instance == null) instance = new NullChainElement();
+                //return instance;
+
+                //thread safe and fast
+                return Nested.instance;
             }
+        }
+        private class Nested
+        {
+            static Nested(){}
+            internal static readonly NullChainElement instance = new NullChainElement();
         }
         public T As<T>(T defaultValue) where T : class
         {
